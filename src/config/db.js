3e1c3a64a326d0616ea6299cc7a9a41f68ba2cce
httpsghsthakur <2024a1r176@mietjammu.in>
2026-07-26 21:10:13 +0530

    Allow CUSTOM_DATABASE_URL to bypass Render lock

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,9 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = process.env.DATABASE_URL 
-  ? new Sequelize(process.env.DATABASE_URL, {
+const dbUrl = process.env.CUSTOM_DATABASE_URL || process.env.DATABASE_URL;
+
+const sequelize = dbUrl 
+  ? new Sequelize(dbUrl, {
         dialect: 'postgres',
         logging: false,
         pool: {

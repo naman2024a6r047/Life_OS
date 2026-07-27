c39892c4fe67e7120a7b76b6f7ec02de1907b3df
httpsghsthakur <2024a1r176@mietjammu.in>
2026-07-27 19:55:06 +0530

    Added Chat System

--- src/routes/chatRoutes.js
+++ src/routes/chatRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const chatController = require('../controllers/chatController');
-const { protect } = require('../middlewares/authMiddleware');
+const protect = require('../middlewares/authMiddleware');
 
 router.get('/:partnerId', protect, chatController.getChatHistory);
 router.put('/:partnerId/read', protect, chatController.markAsRead);
